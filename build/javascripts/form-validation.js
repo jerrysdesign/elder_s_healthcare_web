d711e2479ca5f1a4d1649da885d34fe1bd4303a7
jerrysdsign <jerry7722@hotmail.com>
2015-06-16 14:31:34 +0800

    build 20150616 1431

--- build/javascripts/form-validation.js
+++ build/javascripts/form-validation.js
@@ -1,15 +1,77 @@
 $(function() {
-  var hasSuccess, warningAuto;
+  var apnMsg, disabledBtn, errorAuto, forgetpasswordHide, forgetpasswordShow, loginHide, loginMsg, loginShow, msgHide, msgIcon, rePwMsg, successAuto, warningAuto;
 
-  $('.alert-autocloseable-success').hide();
-  $('.alert-autocloseable-warning').hide();
-  $('.alert-autocloseable-danger').hide();
-  $('.alert-autocloseable-info').hide();
-  warningAuto = function() {
-    $('.alert-autocloseable-warning').show();
-    $('.alert-autocloseable-warning').delay(3000).fadeOut('slow');
+  $('#msg').hide();
+  loginMsg = {
+    bk: {
+      error: 'Email帳號或密碼格式錯誤!'
+    },
+    fe: {
+      warning: 'Email帳號或密碼格式錯誤!'
+    }
   };
-  $('.errorsContainer').hide();
+  rePwMsg = {
+    bk: {
+      success: '請到您的信箱收信，以重新設定密碼。',
+      error: 'Invail email address. 無效的電子郵件地址'
+    },
+    fe: {
+      warning: 'Email帳號或密碼格式錯誤!'
+    }
+  };
+  msgIcon = {
+    error: '<i class="fa fa-times"></i>',
+    warning: '<i class="fa fa-exclamation-triangle"></i>',
+    sucess: '<i class="fa fa-check"></i>'
+  };
+  apnMsg = function() {
+    return $('#msg').append();
+  };
+  warningAuto = function(currentFormId) {
+    var $this;
+
+    $this = $('#' + currentFormId).find('#msg');
+    $this.empty().append(msgIcon.warning + rePwMsg.fe.warning);
+    $this.addClass('alert-warning').show();
+    return $this.delay(3000).fadeOut('slow');
+  };
+  errorAuto = function(currentFormId) {
+    var $this;
+
+    $this = $('#' + currentFormId).find('#msg');
+    $this.empty().append(msgIcon.error + rePwMsg.fe.error);
+    $this.addClass('alert-error').show();
+    return $this.delay(3000).fadeOut('slow');
+  };
+  successAuto = function(currentFormId) {
+    var $this;
+
+    $this = $('#' + currentFormId).find('#msg');
+    $this.empty().append(msgIcon.sucess + rePwMsg.bk.sucess);
+    $this.addClass('alert-sucess').show();
+    return $this.delay(3000).fadeOut('slow');
+  };
+  msgHide = function(currentFormId) {
+    var $this;
+
+    $this = $('#' + currentFormId).find('#msg');
+    return $this.hide();
+  };
+  forgetpasswordHide = function() {
+    return $('.js--forget-password-content').fadeOut();
+  };
+  forgetpasswordShow = function() {
+    return $('.js--forget-password-content').delay().fadeIn();
+  };
+  loginHide = function() {
+    return $('.js--login-content').fadeOut();
+  };
+  loginShow = function() {
+    return $('.js--login-content').delay().fadeIn();
+  };
+  disabledBtn = function() {
+    return $('#fopw-submit').attr('disabled', 'disabled');
+  };
   $('#login-form').validate({
     errorClass: 'state-error',
     validClass: 'state-success',
@@ -37,33 +99,36 @@ $(function() {
         regex: '/^[0-9a-zA-Z]+$/'
       }
     },
+    submitHandler: function(url, data, successCallback) {},
     invalidHandler: function(form, validator) {
-      $('.errorsContainer').fadeOut().fadeIn();
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
+      return errorAuto(currentFormId);
     },
     highlight: function(element, errorClass, validClass) {
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
       $(element).closest('.field').addClass(errorClass).removeClass(validClass);
       if (this.numberOfInvalids() > 0) {
-        $('.errorsContainer').show();
-        warningAuto();
+        return warningAuto(currentFormId);
       }
     },
-    unhighlight: function(element, errorClass, validClass) {},
+    unhighlight: function(element, errorClass, validClass) {
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
+      $(element).closest('.field').removeClass(errorClass).addClass(validClass);
+      if (this.numberOfInvalids() > 0) {
+        return msgHide(currentFormId);
+      }
+    },
     errorPlacement: function(error, element) {
-      error.appendTo('#smartErrors');
+      return error.appendTo('#msg');
     }
   });
-  return;
-  hasSuccess = function() {
-    $('.successContainer').fadeIn(500);
-    $('.forgot-password').fadeOut(400);
-    return $('#fopw-submit').attr('disabled', 'disabled');
-  };
-  $('.successContainer').hide();
   $('#forget-password-form').validate({
-    success: 'valid',
-    submitHandler: function() {
-      hasSuccess();
-    },
     errorClass: 'state-error',
     validClass: 'state-success',
     errorElement: 'span',
@@ -80,22 +145,41 @@ $(function() {
       }
     },
     invalidHandler: function(form, validator) {
-      $('.errorsContainer').show();
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
+      return errorAuto(currentForm.id);
     },
     highlight: function(element, errorClass, validClass) {
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
       $(element).closest('.field').addClass(errorClass).removeClass(validClass);
       if (this.numberOfInvalids() > 0) {
-        $('.errorsContainer').show();
+        return warningAuto(currentFormId);
       }
     },
     unhighlight: function(element, errorClass, validClass) {
+      var currentFormId;
+
+      currentFormId = this.currentForm.id;
       $(element).closest('.field').removeClass(errorClass).addClass(validClass);
-      if (this.numberOfInvalids() === 0) {
-        $('.errorsContainer').hide();
+      if (this.numberOfInvalids() > 0) {
+        return msgHide(currentFormId);
       }
     },
     errorPlacement: function(error, element) {
-      error.appendTo('#smartErrors');
+      return error.appendTo('#msg');
     }
+  });
+  $('.js--forgot-password-toggler').click(function(e) {
+    loginHide();
+    forgetpasswordShow();
+    return e.preventDefault;
+  });
+  return $('.js__back-login-toggler').click(function(e) {
+    forgetpasswordHide();
+    loginShow();
+    return e.preventDefault;
   });
 });
