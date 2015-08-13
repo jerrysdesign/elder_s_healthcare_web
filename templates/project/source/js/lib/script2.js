0335ed225d5e1b4b42dc7ce8653ce86094e8887b
cooperchao <cooperchao0320@gmai.com>
2015-08-13 10:06:39 +0800

    平台管理 > 帳號設定 修改 change_supervisor_popup function 為 common_popup_init 調用function 時參數回傳物件名稱即可

--- templates/project/source/js/lib/script2.js
+++ templates/project/source/js/lib/script2.js
@@ -1,16 +1,18 @@
 // change_supervisor_popup
-function change_supervisor_popup(){
-	var $cont = $("#change_supervisor_box");
+function common_popup_init(object){
+	var $cont = $(object);
 	$.fancybox({
+		'content' : $cont,
 		"minWidth": 600,
-		"padding": 0,
-		"closeBtn": false,
-		'content': $cont
+		"padding" : 0,
+		"closeBtn": false
 	});
 }
 
 // Dom Ready
 $(function(){
 	// change_supervisor_popup
-	$("#change_supervisor").on("click",change_supervisor_popup);
+	$("#change_supervisor").click(function(){
+		common_popup_init("#change_supervisor_box")
+	});
 });
